1452d3cd7d80457a8ce73f5ffdb6d5acff084bf8
TaiPtit <vuductaiptit@gmail.com>
2018-05-25 03:27:17 +0700

    complete simple login feature with simple form

---
+++ vejoi/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as ath_view
+
+urlpatterns= [
+	path('',views.index, name = 'askme'),
+	path('login/', ath_view.login, {'template_name':'vejoi/login.html'}, name = 'login'),
+]
