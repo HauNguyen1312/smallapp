a5e45b7beee325db6f4a51334fc5db960ccc48a0
= <=>
2018-05-24 14:17:21 +0700

    Update models

--- vejoi/models.py
+++ vejoi/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-
-class User(models.Models):
+from django.contrib.urls.models import User
+import datetime
+class User(models.Model):
 	name = models.CharField(max_length=100)
 	email = models.EmailField(max_length=200)
-	password = models.passwordField(max_length=100)
-	Dateofbirth = models.DateTimeField('date published')
-class Question(models.Models):
+	password = models.PasswordField(max_length=100)
+	Date_of_birth = models.DateTimeField('date of birth')
+class Question(models.Model):
 	question_text = models.CharField(max_length=500)
 	anonymous = models.CharField(max_length=10, blank=True, null=True)
-class Answer(models.Models):
+class Answer(models.Model):
 	answer_text = models.CharField(max_length=500)
 # class Question(models.Models):
 # 	question_text = models.CharField(max_length=500)
