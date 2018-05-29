0756f918da3cd6751503ece8e6d4d1ba8cab00e3
TaiPtit <vuductaiptit@gmail.com>
2018-05-29 10:17:12 +0700

    edit models

--- vejoi/models.py
+++ vejoi/models.py
@@ -1,15 +1,16 @@
+from project.models import BaseModel
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
 
-class Question(models.Model):
-	questioner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questioner')
-	responser_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='responser')
-	question_text = models.CharField(max_length=500)
-	anonymous = models.CharField(max_length=10, blank=True, null=True)
-	pub_date = models.DateTimeField('date published')
 
-class Answer(models.Model):
-	question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-	answer_text = models.CharField(max_length=500)
-	answer_date = models.DateTimeField('date published')
+class Question(BaseModel):
+	name = models.CharField('Name of asker', max_length=255, default = '')
+	text = models.TextField('Question')
+	answer = models.TextField(blank=True, null=True)
+	responder_id = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE, 
+		related_name='responder')
+
+	def __str__(self):
+		return self.text
