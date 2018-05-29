f47ec4b7807d2a2722ca0505dbd79e31647d10ef
TaiPtit <vuductaiptit@gmail.com>
2018-05-29 14:48:05 +0700

    edit BaseModel

--- project/models.py
+++ project/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from model_utils.models import SoftDeletableModel, TimeStampedModel
 
 
-class BaseModel(SoftDeletableModel, TimeStampedModel):
+class BaseModel(SoftDeletableModel, TimeStampedModel, models.Model):
     class Meta:
         abstract = True
